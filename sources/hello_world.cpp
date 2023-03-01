0cc63b98d08d14ccfbb89f84cd26809356650797
Igor50591 <igor.nov5059@yandex.ru>
2023-03-01 14:39:05 +0300

    Added comments to the hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main(){
-std::string name;
-std::cout << "Enter name>> ";
-std::cin >> name;
-std::cout << "Hello world from " << name <<"!";
+std::string name; // Объявление строки с именем (@name)
+std::cout << "Enter name>> "; // Вывод запроса имени (@name)
+std::cin >> name; // Получение имени (@name) с клавиатуры 
+std::cout << "Hello world from " << name <<"!"; //Печать сообщения Hello world from @name 
 return 0;
 }
