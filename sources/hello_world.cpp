44679a8e46abe8ca025cf417f57171ddbeb0fcac
Igor50591 <114222139+Igor50591@users.noreply.github.com>
2023-03-01 15:18:08 +0300

    Translated comments into English

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main(){
-std::string name; // Объявление строки с именем (@name)
-std::cout << "Enter name>> "; // Вывод запроса имени (@name)
-std::cin >> name; // Получение имени (@name) с клавиатуры 
-std::cout << "Hello world from " << name <<"!"; //Печать сообщения Hello world from @name 
+std::string name; // Declaring a string with a name (@name)
+std::cout << "Enter name>> "; // Name request output (@name)
+std::cin >> name; // Getting a name (@name) from the keyboard
+std::cout << "Hello world from " << name <<"!"; // Printing the Hello world from @name message
 return 0;
 }
